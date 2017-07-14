7c2957c735e08cf2baf9d137487c8848efaba883
XinLi96 <291750956@qq.com>
2017-07-14 13:45:12 +0800

    解决用iview实现，修改表格数据

--- src/views/order/order3.vue
+++ src/views/order/order3.vue
@@ -6,16 +6,32 @@
                 <Page :total="100" :current="1" @on-change="changePage"></Page>
             </div>
         </div>
+        <!-- modalEdit -->
+        <Modal
+            v-model="modalEdit"
+            title="Edit"
+            ok-text="修改"
+            cancel-text="Cancel"
+            v-on:on-ok="editSucc()">
+            <Form :label-width="50">
+              <Form-item v-for="(value, key) in dataEdit" :key="dataEdit.id">
+                <Input v-model="dataEdit[key]" :placeholder="'Please enter' + key"></Input>
+              </Form-item>
+            </Form> 
+        </Modal>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
+                modalEdit: false,
+                dataEdit: {},
                 tableData1: this.mockTableData1(),
                 tableColumns1: [
                     {
                         title: '姓名',
+                        sortable: true,
                         key: 'name'
                     },
                     {
@@ -27,6 +43,7 @@
                     },
                     {
                         title: '生成时间',
+                        sortable: true,
                         key: 'update',
                         render: (h, params) => {
                             return h('div', this.formatDate(this.tableData1[params.index].update));
@@ -84,7 +101,10 @@
                                     },
                                     on: {
                                         click: () => {
-                                            this.edit(params.index)
+                                            for (let i in params.row) {
+                                                this.dataEdit[i] = params.row[i];
+                                            }
+                                            this.modalEdit = true 
                                         }
                                     }
                                 }, '修改'),
@@ -121,32 +141,31 @@
             show (index) {
                 this.$Modal.info({
                     title: '用户信息',
-                    content: `姓名：${this.tableData1[index].name}`
+                    content: `姓名：${this.tableData1[index].name}<br>地址：${this.tableData1[index].time}<br>生成时间：${this.tableData1[index].update}`
                 })
-                console.log(index);
+                // console.log(index);
             },
             remove (index) {
                 this.tableData1.splice(index, 1);
                 console.log(index);
             },
             edit (index) {
+                this.editName = this.tableData1[index].name;
+                this.editAdd = this.tableData1[index].time;
                 this.$Modal.confirm({
-                    render: (h) => {
-                        return h('Input', {
-                            props: {
-                                value: this.tableData1[index].name,
-                                // placeholder: 'Please enter your name...'
-                            },
-                            on: {
-                                input: (val) => {
-                                    this.value = val;
-                                }
-                            }
-                        })
+                    title: '修改信息',
+                    content: `姓名：<input type="text" style="margin-bottom:20px;" value=${this.editName} v-model="editName"><br>地址：<input type="text" value=${this.editAdd} v-model="editAdd">`,
+                    okText: '修改',
+                    onOk: ()=>{
+                        this.editSucc(index);
                     }
-                })
+                });
+            },
+            editSucc () {
+                console.log(this.dataEdit);
+                this.tableData1[this.dataEdit._index].name = this.dataEdit.name;
+                this.tableData1[this.dataEdit._index].time = this.dataEdit.time;
             }
-            
         }
     }
 </script>

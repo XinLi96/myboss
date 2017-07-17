26dfc5c4ba37c2db820f17419468fd1f482993e0
XinLi96 <291750956@qq.com>
2017-07-17 15:43:08 +0800

    使用element，ce-leader的部分页面

---
+++ src/views/element/main.vue
@@ -0,0 +1,253 @@
+<template>
+    <div class="con">
+        <div class="head">
+            <div class="block st-left">
+                <el-date-picker
+                        v-model="startTime"
+                        type="datetime"
+                        placeholder="起始时间">
+                </el-date-picker>
+            </div>
+            <div class="block" style="display: inline-block">
+                <el-date-picker
+                        v-model="endT"
+                        type="datetime"
+                        placeholder="结束时间">
+                </el-date-picker>
+            </div>
+            <div class="state-select">
+                <span style="font-size: 14px;color: #333333;margin-right: 10px">状态</span>
+                    <el-select v-model="value" placeholder="全部" style="width: 95px">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                        </el-option>
+                    </el-select>
+                <el-button type="primary" style="margin-left: 10px">查询</el-button>
+            </div>
+            <div class="search">
+                <el-input
+                        placeholder="请输入内容"
+                        icon="search"
+                        v-model="input2"
+                        :on-icon-click="handleIconClick">
+                </el-input>
+            </div>
+        </div>
+        <el-table
+            :data="tableData"
+            border
+            stripe="true"
+            style="width: 100%">
+        <el-table-column
+                fixed
+                prop="date"
+                label="商机单号"
+                width="150">
+        </el-table-column>
+        <el-table-column
+                prop="name"
+                label="客户名称"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="province"
+                label="联系人"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="city"
+                label="手机"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="address"
+                label="地区"
+                width="300">
+        </el-table-column>
+        <el-table-column
+                prop="zip"
+                label="创建人"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="zip"
+                label="创建时间"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="zip"
+                label="分配区域"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="zip"
+                label="分配商务"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="zip"
+                label="状态"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                fixed="right"
+                label="操作"
+                width="100">
+            <template scope="scope">
+                <el-button @click="handleClick" type="text" size="small">线索</el-button>
+                <el-button type="text" size="small">回访</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+        <div class="page">
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage4"
+                    :page-sizes="[10, 20, 30, 40]"
+                    :page-size="10"
+                    layout="total, sizes, prev, pager, next"
+                    :total="3529">
+            </el-pagination>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleClick() {
+        console.log(1);
+    }
+    },
+
+    data() {
+        return {
+            tableData: [{
+                date: '2016-05-03',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1518 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-02',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1518 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1518 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1518 弄',
+                zip: 200333
+            },
+                {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    province: '上海',
+                    city: '普陀区',
+                    address: '上海市普陀区金沙江路 1518 弄',
+                    zip: 200333
+                }
+            ],
+            startTime: '',
+            endTime: '',
+            options: [{
+                value: '选项1',
+                label: '黄金糕'
+            }, {
+                value: '选项2',
+                label: '双皮奶'
+            }, {
+                value: '选项3',
+                label: '蚵仔煎'
+            }, {
+                value: '选项4',
+                label: '龙须面'
+            }, {
+                value: '选项5',
+                label: '北京烤鸭'
+            }],
+            value: ''
+        }
+    }
+    }
+</script>
+
+<style>
+    .con{
+        margin-top: 97px;
+        margin-left: 20px;
+        font-family: 微软雅黑;
+    }
+    .head{
+        margin-bottom: 20px;
+    }
+    .st-left{
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .state-select{
+        display: inline-block;
+        margin-left: 40px;
+    }
+    .state-select>span{
+        font-family: 微软雅黑;
+    }
+    .search{
+        float: right;
+    }
+    .page{
+        float: right;
+        margin-top: 20px;
+    }
+</style>
---
+++ src/views/element/opportunity-details.vue
@@ -0,0 +1,168 @@
+<template>
+    <div class="con">
+        <div class="basic-info">
+            <div class="head">基本信息</div>
+            <div class="bi-con">
+                <div class="col1">
+                    <span>客户名称：&nbsp;中企动力科技股份有限公司</span>
+                    <span class="row12">地址：&nbsp;北京市大兴区地盛西路号数码庄园号数码庄园</span>
+                    <span class="row13">联系人：&nbsp;张三丰&nbsp;男</span>
+                    <span class="row14">职位：&nbsp;经理</span>
+                </div>
+                <div class="col1" style="margin-top: 20px">
+                    <span>手机：&nbsp;19216881423</span>
+                    <span class="row12">固话：&nbsp;-</span>
+                    <span class="row13">QQ：&nbsp;-</span>
+                    <span class="row14">微信：&nbsp;-</span>
+                </div>
+                <div class="col1" style="margin-top: 20px">
+                    <span>邮箱：&nbsp;-</span>
+                    <span class="row12">行业：&nbsp;制造业/农副食品加工业</span>
+                    <span class="row13">主营业务：&nbsp;制作鱼罐头</span>
+                </div>
+                <div class="col2 row1" style="position: relative">
+                </div>
+            </div>
+        </div>
+        <div class="ask-product">
+            <div class="head">咨询产品</div>
+            <div class="ap-con">
+                <div style="margin-top: 20px">
+                    <span>建站类</span>
+                    <span class="ap-row12">预算：<span style="color: red">30000元</span></span>
+                    <span class="ap-row13">交付周期：一个月</span>
+                    <span class="ap-row14">电商类</span>
+                    <span class="ap-row15">预算：<span style="color: red">30000元</span></span>
+                    <span class="ap-row16">交付周期：一个月</span>
+                </div>
+                <div style="margin-top: 20px">
+                    <span>推广类</span>
+                    <span class="ap-row12">预算：<span style="color: red">30000元</span></span>
+                    <span class="ap-row13">交付周期：一个月</span>
+                    <span class="ap-row14">管理类</span>
+                    <span class="ap-row15">预算：<span style="color: red">30000元</span></span>
+                    <span class="ap-row16">交付周期：一个月</span>
+                </div>
+                <div style="margin-top: 20px">
+                    <span>解决方案</span>
+                    <span class="ap-row12">预算：<span style="color: red">30000元</span></span>
+                    <span class="ap-row13">交付周期：一个月</span>
+                </div>
+                <div style="margin-top: 20px">
+                     客户需求：<span class="ap-need">比较着急，需要尽快制作完成比较着急，需要尽快制作完成比较着急，需要尽快制作完成比较着急，需要尽快制作完成比较着急，需要尽快制作完成比较着急，需要尽快制作完成比较着急</span>
+                </div>
+            </div>
+        </div>
+        <div class="other">
+            <div class="head">其他</div>
+            <div style="margin-left: 60px;">
+                <div style="margin-top: 10px">
+                    <span>备注：无</span><br>
+                </div>
+                <div style="margin-top: 20px">
+                    <span style="margin-top: 20px">回访：客户回访文字，客户回访文字，客户回访文字，客户回访文字，客户回访文字，客户回访文字</span>
+                </div>
+            </div>
+        </div>
+        <div class="handle-info">
+            <div class="head">操作信息</div>
+            <div class="hi-con" style="margin-left: 60px">
+                <div style="margin-top: 20px">
+                    <span>创建人：张三</span>
+                    <span style="position: absolute;left: 300px">创建时间：2017-07-11 15:25</span>
+                </div>
+                <div style="margin-top: 20px">
+                    <span>状态：成交</span>
+                    <span style="position: absolute;left: 300px">分配区域：北京</span>
+                    <span style="position: absolute;left: 600px">分配商务：张三</span>
+                </div>
+            </div>
+        </div>
+        <div class="cut-line"></div>
+        <el-button style="margin-left: 20px;width: 88px;margin-top: 22px">返回</el-button>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style>
+    .con{
+        font-family: 微软雅黑;
+    }
+    .basic-info{
+        margin-top: 20px;
+        margin-left: 20px;
+    }
+    .head{
+        font-size: 15px;
+        font-weight: 800;
+    }
+    .bi-con{
+        margin-top: 20px;
+    }
+    .col1{
+        margin-left: 60px;
+    }
+    .row12{
+        position: absolute;
+        left: 310px;
+    }
+    .row13{
+        position: absolute;
+        left: 630px;
+    }
+    .row14{
+        position: absolute;
+        left:850px;
+    }
+    .ask-product{
+        margin-top: 30px;
+        margin-left: 20px;
+    }
+    .ap-con{
+        margin-left: 60px;
+    }
+    .ap-row12{
+        position: absolute;
+        left:148px;
+    }
+    .ap-row13{
+        position: absolute;
+        left:258px;
+    }
+    .ap-row14{
+        position: absolute;
+        left:400px;
+    }
+    .ap-row15{
+        position: absolute;
+        left:448px;
+    }
+    .ap-row16{
+        position: absolute;
+        left:558px;
+    }
+    .ap-need{
+        display: inline-block;
+        width: 520px;
+        height: 50px;
+        vertical-align: top;
+        /*border: 1px solid #000;*/
+    }
+    .other{
+        margin-top: 30px;
+        margin-left: 20px;
+    }
+    .handle-info{
+        margin-top: 30px;
+        margin-left: 20px;
+    }
+    .cut-line{
+        width: 100%;
+        height: 5px;
+        margin-top: 165px;
+        /*background-color: #c0ccda;*/
+        box-shadow: 10px 10px 5px #e5e5e5;
+    }
+</style>
